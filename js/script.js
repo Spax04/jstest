56dafb4fcce89b412f1ed9a0e486a06982efb37c
Alex <gotlib14@gmail.com>
2022-09-16 11:16:14 +0300

    first comit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+"use strict";
+
+let obj = {
+    name: "Alex",
+    age: 24,
+   isDeveloper: true
+};
+
+let a;
+
+console.log(obj.name + obj.age + obj.isDeveloper);
+console.log(a);
+
+const arr = [1,2,3,4,"5"];
+
+
+
+
+const newObj={
+    a:1,
+    c:3,
+    b:4
+};
+console.log(newObj.b);
+newObj.b = 2;
+
+console.log(newObj.b);
+
+let result = confirm("R U GAY?");
+
+while(result == false){
+    result = confirm("R U GAY?");
+}
+
+let promis = +prompt("Are you 18 years old?","афц");
+
+console.log(typeof(promis));
+
+let aa = 10;
+let bb = 10;
+
+aa--;
+++bb;
+
+console.log(aa);
+console.log(bb);
+console.log("========");
+console.log(aa);
+console.log(bb);
