019674f3f2c9d5d2ee4577ece6375631bd84e104
Spax04 <gotlib14@gmail.com>
2022-09-17 16:49:20 +0300

    start project. first commit

--- js/script.js
+++ js/script.js
@@ -1,49 +1,19 @@
 "use strict";
 
-let obj = {
-    name: "Alex",
-    age: 24,
-   isDeveloper: true
+let numberOfFilms = +prompt("How many films you sew?");
+
+const personalMovieDB = {
+
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    private : false
 };
 
-let a;
+let lastMovieName = prompt("How names last movie you have seen?");
+let lastMovieRate = prompt("How do you rate it?");
 
-console.log(obj.name + obj.age + obj.isDeveloper);
-console.log(a);
+personalMovieDB.movies[lastMovieName] = lastMovieRate;
 
-const arr = [1,2,3,4,"5"];
-
-
-
-
-const newObj={
-    a:1,
-    c:3,
-    b:4
-};
-console.log(newObj.b);
-newObj.b = 2;
-
-console.log(newObj.b);
-
-let result = confirm("R U GAY?");
-
-while(result == false){
-    result = confirm("R U GAY?");
-}
-
-let promis = +prompt("Are you 18 years old?","афц");
-
-console.log(typeof(promis));
-
-let aa = 10;
-let bb = 10;
-
-aa--;
-++bb;
-
-console.log(aa);
-console.log(bb);
-console.log("========");
-console.log(aa);
-console.log(bb);
+console.log(personalMovieDB);
